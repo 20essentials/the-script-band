6d26c5b08c0c168562efd2b28c52999ce7947b05
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:09:15 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'THE SCRIPT';
+export let nameSongs = [
+	"SUPERHEROES", /*1*/
+	"HALL OF FAME", /*2*/
+	"BREAKEVEN", /*3*/
+	"THE MAN WHO CAN'T BE MOVED", /*4*/
+	"IF YOU COULD SEE ME NOW", /*5*/
+	"NO GOOD IN GOODBYE", /*6*/
+	"FOR THE FIRST TIME", /*7*/
+	"NOTHING", /*8*/
+	"SCIENCE AND FAITH", /*9*/
+	"FLARES", /*10*/
+	"IF YOU EVER COME BACK WITH", /*11*/
+	"LONG GONE AND MOVED", /*12*/
+	"TALK YOU DOWN", /*13*/
+	"MAN ON A WIRE", /*14*/
+	"GLOWING", /*15*/
+	"HURRICANES", /*16*/
+	"BROKEN ARROW", /*17*/
+	"KALEDOISCOPE", /*18*/
+	"SIX DEGREES OF SEPARATION", /*19*/
+	"", /*20*/
+]
